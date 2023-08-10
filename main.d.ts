8799b6af2b397f5676db92d4c87d2ab35f3e1d47
Misha Kanai <misha.a.kanai@gmail.com>
2023-08-10 14:02:12 -0400

    Fix typing of formatInstant to use string instead of number.

--- main.d.ts
+++ main.d.ts
@@ -1,7 +1,17 @@
 declare module "java-js-dateformat" {
-    export const Formatters: {
-        formatLocalDate(year: number, month: number, day: number, pattern: string, locale?: string): string;
-        formatInstant(epochMS: number, pattern: string, locale?: string, zone?: string): string;
-    }
-  }
-  
+  export const Formatters: {
+    formatLocalDate(
+      year: number,
+      month: number,
+      day: number,
+      pattern: string,
+      locale?: string
+    ): string;
+    formatInstant(
+      epochMS: string,
+      pattern: string,
+      locale?: string,
+      zone?: string
+    ): string;
+  };
+}
